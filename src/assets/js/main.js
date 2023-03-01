2630afc235a4eae0de5464bb9a6f0d120a29d9ab
ouhouh23 <dexter32103210@gmail.com>
2023-03-01 11:54:53 +0300

    fix: script load

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,66 +6,10 @@ import { InputDropdown } from './components/inputDropdown.js'
 import { Tabs } from './components/tabs.js'
 import { Toggles } from './components/toggles.js'
 import { Tooltips } from './components/tooltips.js'
-// import { Carousels } from './components/carousels.js'
+import { Carousels } from './components/carousels.js'
 import { Counters } from './components/counters.js'
 import { Shop } from './components/shop.js'
 import { Sliders } from './components/sliders.js'
-
-class Carousels {
-  constructor (
-    block,
-    buttonLeft,
-    buttonRight,
-    blockBody,
-    blockItem
-  ) {
-    this._block = block
-    this._buttonLeft = buttonLeft
-    this._buttonRight = buttonRight
-    this._blockBody = blockBody
-    this._blockItem = blockItem
-  }
-
-  initCarousel (start, inversion, imageWidth) {
-    const blocks = document.querySelectorAll(this._block)
-
-    if (blocks === null) {
-      return
-    }
-
-    blocks.forEach(element => {
-      const toLeft = element.querySelector(this._buttonLeft)
-      const toRight = element.querySelector(this._buttonRight)
-      const blockGallery = element.querySelector(this._blockBody)
-      const items = blockGallery.querySelectorAll(this._blockItem)
-      const itemsAmount = items.length
-      let itemCounter = ~~window.getComputedStyle(blockGallery).getPropertyValue(start)
-      let blockInversion = ~~window.getComputedStyle(blockGallery).getPropertyValue(inversion)
-
-      toRight.addEventListener('click', () => {
-        if (itemCounter < itemsAmount) {
-          const itemWidth = ~~window.getComputedStyle(blockGallery).getPropertyValue(imageWidth)
-
-          blockInversion = blockInversion - itemWidth
-          blockGallery.style.transform = `translateX(${blockInversion}%)`
-          itemCounter += 1
-        }
-      })
-
-      toLeft.addEventListener('click', () => {
-        if (itemCounter > 1) {
-          const itemWidth = ~~window.getComputedStyle(blockGallery).getPropertyValue(imageWidth)
-
-          blockInversion = blockInversion + itemWidth
-          blockGallery.style.transform = `translateX(${blockInversion}%)`
-          itemCounter -= 1
-        }
-      })
-    })
-  }
-}
-
-
 
 const allert = new Close('.allert', '.allert__icon_closed')
 const modal = new Close('.modal', '.allert__icon_closed')
